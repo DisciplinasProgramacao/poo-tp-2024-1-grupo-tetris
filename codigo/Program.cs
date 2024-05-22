e23ab1ad1c0230c3810dbc0a79b5223aa6ebc132
apoliveirapuc <apoliveira@sga.pucminas.br>
2024-05-21 23:13:41 -0300

    Ajustes iniciais.

---
+++ codigo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoPratico
+{
+    public class Program
+    {
+        private static Cliente cadastrarCliente()
+        {
+
+        }
+
+        private static Requisicao criarRequisicao()
+        {
+
+        }
+
+        static void alocarRequisicao()
+        {
+
+        }
+
+        static void finalizarRequisicao()
+        {
+
+        }
+
+        static Requisicao atualizarFila() 
+        { 
+        
+        }
+
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
