ea257acdb873f77e70c97e01114976e29d2ec051
Pedro Passos PUC <101272935+pedroppassoss@users.noreply.github.com>
2024-05-29 12:09:32 -0300

    Insert e Update  Cardapio.cs
    Insert do Método adicionarProduto(Produto produto).
    Update do Método apresentarCardapio()

--- codigo/Cardapio.cs
+++ codigo/Cardapio.cs
@@ -10,18 +10,33 @@ namespace TrabalhoPratico
     {
         private List<Produto> itens;
 
+        /// <summary>
+        /// Construtor da classe Cardapio.
+        /// </summary>
         public Cardapio()
         {
             itens = new List<Produto>();
         }
 
+        /// <summary>
+        /// Adiciona um novo produto ao cardápio.
+        /// </summary>
+        /// <param name="produto">Produto a ser adicionado.</param>
+        public void AdicionarProduto(Produto produto)
+        {
+        itens.Add(produto);
+        }
+
+        /// <summary>
+        /// Mostra todos os produtos cadastrados no cardápio.
+        /// </summary>
         public string apresentarCardapio()
         {
-            foreach (Produto item in itens) 
-            {
-                minhaString += (idProduto) + " - " + item.ToString();
-                return minhaString;
-            }
+           Console.WriteLine("----- Cardápio -----");
+        foreach (var produto in itens)
+        {
+            Console.WriteLine($"ID: {produto.IdProduto} | Descrição: {produto.Descricao} | Valor: {produto.Valor:C2}");
+        }
         }
     }
 }
